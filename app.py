c28ea580a8fa7ff10093522782c3166ce11f9ce8
AmirFone <ahossain20@fordham.edu>
2023-12-31 20:18:07 -0500

    Update app.py for Heroku deployment

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from config import get_config
 from routes import register_routes
-from flask_cors import CORS
 
-def create_app():
-    app = Flask(__name__, static_url_path="")
-    CORS(app)
-    app.config.update(get_config())
-    register_routes(app)
-    return app
+app = Flask(__name__, static_url_path="")
+CORS(app)
+app.config.update(get_config())
+register_routes(app)
 
-app = create_app()
-
-def lambda_handler(event, context):
-    # Logic to handle the Lambda event and return a response
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Lambda!'
-    }
+if __name__ == "__main__":
+    app.run()
